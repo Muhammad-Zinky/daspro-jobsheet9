3e80c542158076ab02ff10173cc346fe037924c3
mohammad zinky <mohammadzinky@gmail.com>
2024-11-15 16:29:10 +0700

    js9

---
+++ ArrayBilangan10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ArrayBilangan10{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] bil = {5, 13, -7, 17};
+        
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
